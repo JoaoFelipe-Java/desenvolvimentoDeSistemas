40efd12ea705c59eb98b2ee0bace4250466cbe15
JoaoFelipe-Java <106123370+JoaoFelipe-Java@users.noreply.github.com>
2024-11-12 09:33:46 -0300

    v0,001
    atualização

--- print.py
+++ print.py
@@ -5,6 +5,12 @@ DOCSTRING
 # escreva
 print("Hello word!")
 # leia
-nomePessoa = input('Digite seu nome')
-
+nomePessoa = input('Digite seu nome') # Pede um dado para o usuario
+print('Meu', 'nome', 'é ',sep='-') # sep (separa as strings com um caracter que voce escolher).
 print(nomePessoa)
+
+# type(mostra o tipo de um dado/variavel)
+print(type(11)) # int (numero inteiro)
+print(type(1.1)) # float (numero com uma parte decimal)
+print(type('11')) # string (conjunto de caracteres)
+print(type(True)) # bool (operador logico verdadeiro ou falso)
