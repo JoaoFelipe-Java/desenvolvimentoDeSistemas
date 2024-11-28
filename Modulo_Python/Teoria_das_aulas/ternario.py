0ef2689c3befe5800994ac7f14ebd7c1096a19f0
JoaoFelipe-Java <106123370+JoaoFelipe-Java@users.noreply.github.com>
2024-11-28 08:23:16 -0300

    att

--- Modulo_Python/Teoria_das_aulas/ternario.py
+++ Modulo_Python/Teoria_das_aulas/ternario.py
@@ -1,9 +1,25 @@
+"""
+Atividade01: Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10)
+"""
 #solução do Copilot
 [print(f'8 x {i} = {8 * i}') for i in range(1, 11)]
 
 # Minha solução
 num = 8
 tabuada = lambda numero, x: numero * x
+
 for i in range (10):
     print(f'{num}x{i+1}={tabuada(num,i+1)}')
+    
+"""
+Atividade02: Faça um algoritmo que avalie se o usuario e senha 
+cadastrados e se não tiver, printe uma falha
+senao printe que deu tudo certo
+(considerar que usuario e senha sejam ''ADM')
+"""
+usuario = 'ADM'
+senha = 'ADM'
 
+valida_user = lambda user, senha: user == 'ADM' and senha == 'ADM'
+
+print('Logado') if valida_user(usuario,senha) == True else print('erro')
